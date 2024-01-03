7d56fa0394e6d6f870b97ebf974836c430f2d8db
TheSchipper <107776081+TheSchipper@users.noreply.github.com>
2024-01-03 17:54:54 -0500

    Update methods

--- hcp_vault_secrets_client/hcp.py
+++ hcp_vault_secrets_client/hcp.py
@@ -16,6 +16,28 @@ class HcpClient:
                             f"projects/{self.project_id}/apps/{self.project_name}")
         self.request_headers = {"Authorization": f"Bearer {self.access_token}"}
 
+    def create_app_secret(self, secret_name, secret_value):
+        url = (f"{HCP_API_URL}/secrets/2023-06-13/organizations/{self.organization_id}/"
+               f"projects/{self.project_id}/apps/{self.project_name}/kv")
+        body = {"name": secret_name, "value": secret_value}
+        resp = requests.post(url, headers=self.request_headers, data=json.dumps(body))
+        resp.raise_for_status()
+        return resp.json()
+
+    def get_app_secret(self, secret_name):
+        url = (f"{HCP_API_URL}/secrets/2023-06-13/organizations/{self.organization_id}/"
+               f"projects/{self.project_id}/apps/{self.project_name}/secrets/{secret_name}")
+        resp = requests.get(url, headers=self.request_headers)
+        resp.raise_for_status()
+        return resp.json()
+
+    def delete_app_secret(self, secret_name):
+        url = (f"{HCP_API_URL}/secrets/2023-06-13/organizations/{self.organization_id}/"
+               f"projects/{self.project_id}/apps/{self.project_name}/secrets/{secret_name}")
+        resp = requests.delete(url, headers=self.request_headers)
+        resp.raise_for_status()
+        return resp.json()
+
     def get_app(self):
         resp = requests.get(self.api_app_url, headers=self.request_headers)
         resp.raise_for_status()
